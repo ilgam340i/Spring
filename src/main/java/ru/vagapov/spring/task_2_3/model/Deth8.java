ac32f935efa1c9909c7557b574aed6b1f128e80e
Vadim Vagapov <midav1903@mail.ru>
2024-09-07 18:13:17 +0500

    second commit

--- src/main/java/ru/vagapov/spring/task_2_3/model/Deth8.java
+++ src/main/java/ru/vagapov/spring/task_2_3/model/Deth8.java
@@ -1,4 +1,8 @@
 package ru.vagapov.spring.task_2_3.model;
 
 public class Deth8 {
+    @Override
+    public String toString() {
+        return ":(";
+    }
 }
--- src/main/java/ru/vagapov/spring/task_2_3/model/Needle7.java
+++ src/main/java/ru/vagapov/spring/task_2_3/model/Needle7.java
@@ -3,6 +3,6 @@ package ru.vagapov.spring.task_2_3.model;
 public class Needle7 {
     @Override
     public String toString() {
-        return ", смерть Кощея на игле :( " + "";
+        return ", смерть Кощея на игле ";
     }
 }
